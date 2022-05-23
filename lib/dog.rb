058c01de417c78f16cd5dbc88eed4b0c1fc4e2e5
Hyun Sung Cho <hcho774@gmail.com>
2022-05-23 12:25:17 -0400

    commit tree 7f90dea9c5c81875aa3608338f7dfb04f3b316ce

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
+require "pry"
 class Dog
+    attr_reader :name
+    attr_reader :breed
+    def name=(name)
+        @name = name
+    end
 
+    # def name
+    #     @name
+    # end
+
+    def breed=(breed)
+        @breed = breed
+    end
+    
+    # def breed
+    #     @breed
+    # end
 end
+
+mastiff = Dog.new
+mastiff.name= "Mastiff"
+
+
+binding.pry
